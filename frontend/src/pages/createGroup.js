697ebd5dede10a2dbec3586569c007e5c67517b6
subin Bae <subinbae96@gmail.com>
2024-08-17 00:11:25 +0900

    new file

--- frontend/src/pages/createGroup.js
+++
@@ -1,9 +0,0 @@
-import "../components/createGroup.css";
-import { createGroupForm } from "../components/createGroupForm.js";
-import "./createGroup.css";
-
-// 페이지 로드 시 그룹 만들기 폼을 렌더링
-window.onload = () => {
-  const root = document.getElementById("root");
-  root.appendChild(createGroupForm());
-};
--- src/pages/createGroup.js
+++
@@ -1,8 +0,0 @@
-import "../components/createGroupForm.css"; // 폼 스타일
-import { createGroupForm } from "../components/createGroupForm.js";
-import "./createGroup.css"; // 페이지 스타일
-
-window.onload = () => {
-  const root = document.getElementById("root");
-  root.appendChild(createGroupForm());
-};
