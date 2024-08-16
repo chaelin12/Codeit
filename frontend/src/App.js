0e98725a578d0eb6add159d36f490b799d047ffa
subin Bae <subinbae96@gmail.com>
2024-08-17 00:56:32 +0900

    edit App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+import CreateGroup from "./pages/CreateGroup";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/creategroup">CreateGroup</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/creategroup" element={<CreateGroup />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/pages/CreateGroup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function createGroup() {
+  return <h1>그룹 생성 화면</h1>;
+}
+
+export default createGroup;
