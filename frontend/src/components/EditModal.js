9de6b634af77f0cb600015815789db0bf7286b84
subin Bae <subinbae96@gmail.com>
2024-08-30 01:24:27 +0900

    에러

--- frontend/src/components/EditModal.js
+++ frontend/src/components/EditModal.js
@@ -6,8 +6,8 @@ const EditModal = ({ isOpen, closeModal, groupDetail, onSave, groupId }) => {
   const [groupName, setGroupName] = useState(groupDetail.name);
   const [groupImage, setGroupImage] = useState(groupDetail.imageUrl);
   const [groupIntro, setGroupIntro] = useState(groupDetail.introduction);
+  const [groupPassword, setGroupPassword] = useState(groupDetail.password); // 기존 비밀번호
   const [isPublic, setIsPublic] = useState(groupDetail.isPublic);
-  const [editPassword, setEditPassword] = useState(""); // 새로운 변수명으로 변경
   const [errorMessage, setErrorMessage] = useState(""); // For displaying error messages
 
   const handleSave = async () => {
@@ -18,13 +18,13 @@ const EditModal = ({ isOpen, closeModal, groupDetail, onSave, groupId }) => {
         imageUrl: groupImage,
         introduction: groupIntro,
         isPublic,
+        password: groupPassword, // 서버에 이미 존재하는 password를 포함시킴
       };
 
       // 서버에 업데이트 요청을 보내는 부분입니다.
       const response = await axios.put(`/api/groups/${groupId}`, updatedGroup, {
         headers: {
           "Content-Type": "application/json",
-          Authorization: editPassword, // 헤더에 editPassword 값을 포함하여 서버로 보냅니다.
         },
       });
 
@@ -33,14 +33,23 @@ const EditModal = ({ isOpen, closeModal, groupDetail, onSave, groupId }) => {
       closeModal();
     } catch (error) {
       // 에러 상태에 따른 메시지 설정
-      if (error.response.status === 400) {
-        setErrorMessage("잘못된 요청입니다");
-      } else if (error.response.status === 403) {
-        setErrorMessage("비밀번호가 틀렸습니다");
-      } else if (error.response.status === 404) {
-        setErrorMessage("존재하지 않습니다");
+      if (error.response && error.response.status) {
+        switch (error.response.status) {
+          case 400:
+            setErrorMessage("잘못된 요청입니다");
+            break;
+          case 403:
+            setErrorMessage("비밀번호가 틀렸습니다");
+            break;
+          case 404:
+            setErrorMessage("존재하지 않습니다");
+            break;
+          default:
+            setErrorMessage("알 수 없는 오류가 발생했습니다");
+            break;
+        }
       } else {
-        setErrorMessage("알 수 없는 오류가 발생했습니다");
+        setErrorMessage("서버와의 통신 중 오류가 발생했습니다");
       }
     }
   };
@@ -119,8 +128,8 @@ const EditModal = ({ isOpen, closeModal, groupDetail, onSave, groupId }) => {
             <label>수정 권한 인증</label>
             <input
               type="password"
-              value={editPassword}
-              onChange={(e) => setEditPassword(e.target.value)}
+              value={groupPassword} // 수정 권한 인증에 입력된 값은 groupPassword로 설정됩니다.
+              onChange={(e) => setGroupPassword(e.target.value)} // 입력된 비밀번호를 groupPassword 상태로 설정
               placeholder="비밀번호를 입력해 주세요"
             />
           </div>
